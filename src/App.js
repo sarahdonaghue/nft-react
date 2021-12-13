661f701e938201dee188064b8c3fcf5b61a6487f
Sarah Donaghue <sarahmdonaghue@gmail.com>
2021-12-12 20:51:12 -0500

    master

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+import CollectionCard from './components/CollectionCard';
+import Header from './components/Header';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import PunkList from './components/PunkList';
+import Main from './components/Main';
+
+function App() {
+  const [punkListData, setPunkListData] = useState([])
+  const [selectedPunk, setSelectedPunk] = useState(0)
+
+  useEffect(() => {
+    const getMyNfts = async () => {
+      const openseaData = await axios.get('https://testnets-api.opensea.io/assets?order_direction=asc&asset_contract_address=0x613A523f3187Dc5025afe8CA96475ec1460A7a63&order_direction=asc')
+      console.log(openseaData.data.assets)
+      setPunkListData(openseaData.data.assets)
+    }
+
+    return getMyNfts()
+  }, [])
+  
+  return (
+  <div className='app'> 
+    <Header />
+    {punkListData.length > 0 && (
+      <>
+        <Main punkListData={punkListData} selectedPunk={selectedPunk}/>
+        <PunkList punkListData={punkListData} 
+        setSelectedPunk={setSelectedPunk} 
+        />
+      </>
+      )
+    }
+  </div>
+  );
+}
+
+export default App;
